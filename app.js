08c57c599d246d07a354efb3c26b9a4ab87c081d
Sainand43 <kundaikarsainand@gmail.com>
2025-02-06 23:11:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express"; 
+import authRouter from "./src/routes/authRoute.js";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.port || 8000; 
+const app = express();
+
+app.use(express.json()); 
+app.use(cors());
+
+app.use("/auth", authRouter)
+
+app.get("/", (req, res) =>{ res.send("Welcome to the API") })
+
+app.listen(port, () => { 
+    console.log(`Server running on port ${port}`) 
+})
